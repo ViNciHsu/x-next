317f50a6c73b9c5b2a3dca7ace5fc44c8be105b1
Vinci <vinci-tc.hsu@amfc.com.tw>
2024-05-17 16:51:12 +0800

    Add mini profile to the sidebar component

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -3,12 +3,21 @@ import NextAuth from "next-auth/next";
 import GoogleProvidere from "next-auth/providers/google";
 
 const handeler = NextAuth({
-    providers: [
-        GoogleProvidere({
-            clientId: process.env.GOOGLE_CLIENT_ID,
-            clientSecret: process.env.GOOGLE_CLIENT_SECRET
-        })
-    ]
-})
+  providers: [
+    GoogleProvidere({
+      clientId: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
+  ],
+
+  callbacks: {
+    async session({ session, token }) {
+        session.user.username = session.user.name.split(' ').join(' ').toLocaleLowerCase();
+        session.user.uid = token.sub;
+        console.log(session);
+        return session;
+    },
+  },
+});
 
 export { handeler as GET, handeler as POST };
