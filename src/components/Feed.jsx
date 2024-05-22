651029ab650cc2bcb26f735213ed0e13bd011a2e
vc <vc@gmail.com>
2024-05-23 03:10:21 +0800

    Add icons component to the post component

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -5,18 +5,17 @@ import Post from "./Post";
 export default async function Feed() {
 	const db = getFirestore(app);
 	const q = query(collection(db, 'posts'), orderBy('timestamp', 'desc'));
-  const querySnapshot = await getDocs(q);
+	const querySnapshot = await getDocs(q);
 	let data = [];
 	querySnapshot.forEach((doc) => {
-		data.push({id:doc.id, ...doc.data()});
+		data.push({ id: doc.id, ...doc.data() });
 	});
 	console.log(data);
-  return (
-    <div>
-			{data.map((post)=> (
+	return (
+		<div>
+			{data.map((post) => (
 				<Post key={post.id} post={post} id={post.id} />
-				
 			))}
 		</div>
-  )
+	)
 }
