c1cfc7f8d8821acb6e345bb8bf257cbc76f5ab8b
Vinci <vinci-tc.hsu@amfc.com.tw>
2024-05-24 16:53:01 +0800

    Add react-modal package and update CommentModal component

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -18,7 +18,7 @@ import {
 } from "firebase/firestore";
 import { app } from "@/firebase";
 import { useEffect, useState } from "react";
-import { modalState } from "@/components/ModalAtom";
+import { modalState, postIdState } from "@/components/ModalAtom";
 import { useRecoilState } from "recoil";
 
 export default function Icons({ id, uid }) {
@@ -26,6 +26,7 @@ export default function Icons({ id, uid }) {
   const [isLiked, setIsliked] = useState(false);
   const [likes, setLikes] = useState([]);
   const [open, setOpen] = useRecoilState(modalState);
+  const [postId, setPostId] = useRecoilState(postIdState);
   const db = getFirestore(app);
   const likePost = async () => {
     if (session) {
@@ -75,7 +76,14 @@ export default function Icons({ id, uid }) {
     <div className="flex justify-start gap-5 p-2 text-gray-500">
       <HiOutlineChat
         className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100"
-        onClick={() => setOpen(!open)}
+        onClick={() => {
+          if (!session) {
+            signIn();
+          } else {
+            setOpen(!open);
+            setPostId(id);
+          }
+        }}
       />
 
       <div className="flex items-center">
