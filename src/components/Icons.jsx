c00bb7cf6f1535e7628f4f2fc930a1847c66b8a7
Vinci <vinci-tc.hsu@amfc.com.tw>
2024-05-24 13:14:16 +0800

    Update Icons component to handle likes

--- src/components/Icons.jsx
+++ src/components/Icons.jsx
@@ -1,13 +1,78 @@
-'use client';
+"use client";
 
-import { HiOutlineChat, HiOutlineHeart, HiOutlineTrash } from 'react-icons/hi';
+import {
+  HiOutlineChat,
+  HiOutlineHeart,
+  HiOutlineTrash,
+  HiHeart,
+} from "react-icons/hi";
+import { signIn, useSession } from "next-auth/react";
+import {
+  collection,
+  doc,
+  getFirestore,
+  serverTimestamp,
+  setDoc,
+  onSnapshot,
+  deleteDoc,
+} from "firebase/firestore";
+import { app } from "@/firebase";
+import { useEffect, useState } from "react";
 
 export default function Icons({ id, uid }) {
-	return (
-		<div className='flex justify-start gap-5 p-2 text-gray-500'>
-			<HiOutlineChat className='h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100' />
-			<HiOutlineHeart className='h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100' />
-			<HiOutlineTrash className='h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100' />
-		</div>
-	)
+  const { data: session } = useSession();
+  const [isLiked, setIsliked] = useState(false);
+  const [likes, setLikes] = useState([]);
+  const db = getFirestore(app);
+  const likePost = async () => {
+    if (session) {
+      if (isLiked) {
+        await deleteDoc(doc(db, "posts", id, "likes", session?.user.uid));
+      } else {
+        await setDoc(doc(db, "posts", id, "likes", session.user.uid), {
+          username: session.user.username,
+          timestamp: serverTimestamp(),
+        });
+      }
+    } else {
+      signIn;
+    }
+  };
+
+  useEffect(() => {
+    onSnapshot(collection(db, "posts", id, "likes"), (snapshot) => {
+      setLikes(snapshot.docs);
+    });
+  }, [db]);
+
+  useEffect(() => {
+    setIsliked(
+      likes.findIndex((like) => like.id === session?.user?.uid) !== -1
+    );
+  }, [likes]);
+
+  return (
+    <div className="flex justify-start gap-5 p-2 text-gray-500">
+      <HiOutlineChat className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-sky-500 hover:bg-sky-100" />
+      <div className="flex items-center">
+        {isLiked ? (
+          <HiHeart
+            onClick={likePost}
+            className="h-8 w-8 cursor-pointer rounded-full transition text-red-600 duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
+          />
+        ) : (
+          <HiOutlineHeart
+            onClick={likePost}
+            className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100"
+          />
+        )}
+        {likes.length > 0 && (
+          <span className={`text-xs ${isLiked && "text-red-600"}`}>
+            {likes.length}
+          </span>
+        )}
+      </div>
+      <HiOutlineTrash className="h-8 w-8 cursor-pointer rounded-full transition duration-500 ease-in-out p-2 hover:text-red-500 hover:bg-red-100" />
+    </div>
+  );
 }
