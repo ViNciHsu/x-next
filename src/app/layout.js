a82f5cd71f529dcf0e3e530ca202a638d90e22dd
Vinci <vinci-tc.hsu@amfc.com.tw>
2024-05-30 09:40:39 +0800

    Add Search functionality about News

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,32 +1,46 @@
+"use client";
+
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Sidebar from "@/components/Sidebar";
 import News from "@/components/News";
 import SessionWrapper from "@/components/SessionWrapper";
 import CommentModal from "@/components/CommentModal";
+import { useState, useEffect } from "react";
 
 const inter = Inter({ subsets: ["latin"] });
 
-export const metadata = {
-  title: "X Clone",
-  description: "A clone of website built with Next.js and Tailwind CSS",
-};
+// export const metadata = {
+//   title: "X Clone",
+//   description: "A clone of website built with Next.js and Tailwind CSS",
+// };
 
-export const dynamic = 'force-dynamic';
+export const dynamic = "force-dynamic";
 
 export default function RootLayout({ children }) {
+  const [searchQuery, setSearchQuery] = useState("");
+
   return (
     <SessionWrapper>
       <html lang="en">
         <body className={inter.className}>
           <div className="flex justify-between max-w-6xl mx-auto">
-            <div className="hidden sm:inline border-r h-screen"><Sidebar /></div>
+            <div className="hidden sm:inline border-r h-screen">
+              <Sidebar />
+            </div>
             <div className="w-2xl flex-1">{children}</div>
             <div className="lg:flex-col p-3 h-screen border-l hidden lg:flex w-[24rem]">
               <div className="sticky top-0 bg-white py-2">
-                <input type="text" placeholder="Search" className="bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2" />
+                <input
+                  type="text"
+                  placeholder="Search"
+                  className="bg-gray-100 border border-gray-200 rounded-3xl text-sm w-full px-4 py-2"
+                  value={searchQuery}
+                  onChange={(e) => setSearchQuery(e.target.value)}
+                />
               </div>
-              <News /></div>
+              <News searchQuery={searchQuery} />
+            </div>
           </div>
           <CommentModal />
         </body>
--- src/components/News.jsx
+++ src/components/News.jsx
@@ -3,55 +3,77 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-export default function News() {
+export default function News({ searchQuery }) {
   const [news, setNews] = useState([]);
-  const [articleNum, setArticleNum] = useState(3);
+  const [resultNum, setresultNum] = useState(3);
+  const [filteredNews, setFilteredNews] = useState([]);
+
   useEffect(() => {
-    // fetch("https://saurav.tech/NewsAPI/top-headlines/category/business/us.json")
-    //   .then((res) => res.json())
-    //   .then((data) => {
-    //     setNews(data.articles);
-    //   });
     axios
       .get(
-        "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
+        // "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
+        "https://newsdata.io/api/1/latest?apikey=pub_44995dd50daff2cc4007b7627523e2913d3f7&q=台灣"
       )
       .then((response) => {
-        setNews(response.data.articles);
+        console.log(response.data.results);
+        setNews(response.data.results);
+        setFilteredNews(response.data.results);
       })
       .catch((error) => {
         console.error("錯誤資訊:", error);
       });
   }, []);
+
+  useEffect(() => {
+    if (!searchQuery) {
+      setFilteredNews(news);
+      return;
+    }
+
+    const filtered = news.filter((result) =>
+      result.title.toLowerCase().includes(searchQuery.toLowerCase())
+    );
+    setFilteredNews(filtered);
+  }, [searchQuery, news]);
+
   return (
     <div className="text-gray-700 space-y-3 bg-gray-100 rounded-xl pt-2">
       <h4 className="font-bold text-xl px-4">Whats happening</h4>
-      {news.slice(0, articleNum).map((article) => (
-        <div key={article.url}>
-          <a href={article.url} target="_blank">
+      {filteredNews.slice(0, resultNum).map((result) => (
+        <div key={result.link}>
+          <a href={result.link} target="_blank">
             <div className="flex items-center justify-between px-4 py-2 space-x-1 hover:bg-gray-200 transition duration-200">
               <div className="space-y-0.5">
-                <h6 className="text-sm font-bold">{article.title}</h6>
+                <h6 className="text-sm font-bold">{result.title}</h6>
                 <p className="text-xs font-meduium text-gray-500">
-                  {article.source.name}
+                  {result.keywords &&
+                    result.keywords.map((keyword, index) => (
+                      <span key={index}>
+                        {index > 0 && " "}#{keyword}
+                      </span>
+                    ))}
                 </p>
               </div>
               <img
-                src={article.urlToImage}
+                src={result.image_url}
                 width={100}
                 className="rounded-xl"
-                alt=""
+                alt={result.article_id}
               />
             </div>
           </a>
         </div>
       ))}
-      <button
-        onClick={() => setArticleNum(articleNum + 3)}
-        className="text-blue-300 pl-4 pb-3 hover:text-blue-400 text-sm"
-      >
-        Load More
-      </button>
+      {!searchQuery ? (
+        <button
+          onClick={() => setresultNum(resultNum + 3)}
+          className="text-blue-300 pl-4 pb-3 hover:text-blue-400 text-sm"
+        >
+          Load More
+        </button>
+      ) : (
+        ""
+      )}
     </div>
   );
 }
